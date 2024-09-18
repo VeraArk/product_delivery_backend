34ca474f32e9acbed4a96042f5a18f130245da4f
DimitriDiel <diel-d@web.de>
2024-09-18 22:16:47 +0400

    first commit

---
+++ src/main/java/org/product_delivery_backend/ProductDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package org.product_delivery_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDeliveryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductDeliveryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/product_delivery_backend/ProductDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.product_delivery_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
