0e8dda40ff77f2447277053e4e21c764107b9203
Vladimir Gavrilyuk <voovius@yahoo.com>
2024-09-19 10:45:00 +0300

    Initial commit

--- src/main/java/org/product_delivery_backend/ProductDeliveryBackendApplication.java
+++ src/main/java/org/product_delivery_backend/ProductDeliveryBackendApplication.java
@@ -1,8 +1,11 @@
 package org.product_delivery_backend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ProductDeliveryBackendApplication {
 
@@ -10,4 +13,9 @@ public class ProductDeliveryBackendApplication {
         SpringApplication.run(ProductDeliveryBackendApplication.class, args);
     }
 
+    @Autowired
+    public void start(StartToken unused)
+    {
+        log.info("It works.");
+    }
 }
