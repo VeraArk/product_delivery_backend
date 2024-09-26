52edadb7fb2df49bd0ccaac65d92482d9e4aa655
DimitriDiel <diel-d@web.de>
2024-09-26 13:42:43 +0400

    fix properties and some imports

--- src/main/java/org/product_delivery_backend/mapper/ProductMapper.java
+++ src/main/java/org/product_delivery_backend/mapper/ProductMapper.java
@@ -3,21 +3,16 @@ package org.product_delivery_backend.mapper;
 import org.mapstruct.Mapper;
 import org.product_delivery_backend.dto.productDTO.AllProductResponseDto;
 import org.product_delivery_backend.dto.productDTO.ProductRequestDto;
-import org.mapstruct.Mapping;
 import org.product_delivery_backend.dto.productDTO.ProductResponseDto;
 import org.product_delivery_backend.entity.Product;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-
-//    @Mapping(source = "product.title", target = "title")
     AllProductResponseDto toAllProductResponseDTO(Product product);
-
 
     ProductResponseDto toProductResponseDTO(Product product);
 
-//    @Mapping(target = "id", ignore = true)
     Product toProduct(ProductRequestDto productRequestDto);
 
 
