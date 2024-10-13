19805bc5dabbdc6a79c6fc3384dac1a84711dd13
Dimitri Diel <150340219+DimitriDiel@users.noreply.github.com>
2024-10-14 00:31:21 +0400

    add controller and service for getOrders

--- src/main/java/org/product_delivery_backend/DTO/orderDto/OrderResponseDto.java
+++ src/main/java/org/product_delivery_backend/DTO/orderDto/OrderResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import org.product_delivery_backend.dto.OrderProduct.OrderProductResponseDto;
 import org.product_delivery_backend.entity.OrderProduct;
 import org.product_delivery_backend.entity.OrderStatus;
+import org.product_delivery_backend.entity.PaymentMethod;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -19,8 +20,11 @@ public class OrderResponseDto {
         private Long id;
         private Long userId;
         private LocalDateTime orderTime;
+        private String address;
+        private LocalDateTime deliveryTime;
         private List<OrderProductResponseDto> orderProducts;
         private BigDecimal totalSum;
         private OrderStatus orderStatus;
+        private PaymentMethod paymentMethod;
     }
 
--- src/main/java/org/product_delivery_backend/mapper/OrderProductMapper.java
+++ src/main/java/org/product_delivery_backend/mapper/OrderProductMapper.java
@@ -6,13 +6,18 @@ import org.product_delivery_backend.dto.OrderProduct.OrderProductResponseDto;
 import org.product_delivery_backend.entity.CartProduct;
 import org.product_delivery_backend.entity.OrderProduct;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface OrderProductMapper {
 
     @Mapping(target = "order", ignore = true)
     OrderProduct toOrderProduct(CartProduct cartProduct);
 
-    @Mapping(source = "orderProduct.order.id", target = "orderId")
-    @Mapping(source = "orderProduct.product.id", target = "productId")
+    @Mapping(source = "order.id", target = "orderId")
+    @Mapping(source = "product.id", target = "productId")
     OrderProductResponseDto toOrderProductResponseDto (OrderProduct orderProduct);
+
+
+    List<OrderProductResponseDto> toOrderProductResponseDtoList (List<OrderProduct> orderProductList);
 }
